fabfffab395c47546491184021dc4c94547726ea
Tyler Ryden <tryden11@gmail.com>
2023-06-17 16:13:06 -0400

    Decided to remove bottom navigation from MainActivity and have each fragment contain their own bottom app bar

--- app/src/main/java/com/tryden/nook/ui/MainActivity.kt
+++ app/src/main/java/com/tryden/nook/ui/MainActivity.kt
@@ -5,24 +5,44 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import com.tryden.nook.R
 import com.tryden.nook.arch.NookViewModel
 import com.tryden.nook.database.AppDatabase
+import com.tryden.nook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        val bottomNavigationView: BottomNavigationView = binding.bottomToolbar
+
+
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
+        //Add custom tab menu
+//        val bottomMenuView = bottomNavigationView.getChildAt(0) as BottomNavigationMenuView
+//        val view = bottomMenuView.getChildAt(1)
+//        val itemView = view as BottomNavigationItemView
+//
+//        val viewCustom = LayoutInflater.from(this).inflate(R.layout.custom_menu_item_add_folder, bottomMenuView, false)
+//        itemView.addView(viewCustom)
+
+
+//        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+
 
         val viewModel: NookViewModel by viewModels()
         viewModel.init(AppDatabase.getDatabase(this))
+
     }
+
 }
