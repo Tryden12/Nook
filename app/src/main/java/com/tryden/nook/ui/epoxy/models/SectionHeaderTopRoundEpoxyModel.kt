a6d844affb8317a10dae314a3630b941320fa8d8
Tyler Ryden <tryden11@gmail.com>
2023-06-18 09:47:27 -0400

    Created section header rounded and footer rounded layout files & epoxy models, replaced the folder and add folder assets

---
+++ app/src/main/java/com/tryden/nook/ui/epoxy/models/SectionHeaderTopRoundEpoxyModel.kt
@@ -0,0 +1,14 @@
+package com.tryden.nook.ui.epoxy.models
+
+import com.tryden.nook.R
+import com.tryden.nook.databinding.ModelSectionHeaderTopBinding
+import com.tryden.nook.ui.epoxy.ViewBindingKotlinModel
+
+class SectionHeaderTopRoundEpoxyModel : ViewBindingKotlinModel<ModelSectionHeaderTopBinding>
+    (R.layout.model_section_header_top) {
+
+    override fun ModelSectionHeaderTopBinding.bind() {
+        // nothing to do
+    }
+
+}
--- app/src/main/java/com/tryden/nook/ui/home/HomeEpoxyController.kt
+++ app/src/main/java/com/tryden/nook/ui/home/HomeEpoxyController.kt
@@ -1,12 +1,10 @@
 package com.tryden.nook.ui.home
 
 import com.airbnb.epoxy.EpoxyController
-import com.tryden.nook.R
-import com.tryden.nook.database.entity.PriorityItemEntity
-import com.tryden.nook.databinding.ModelItemEntityBinding
-import com.tryden.nook.ui.epoxy.ViewBindingKotlinModel
 import com.tryden.nook.ui.epoxy.models.HeadingPageTitleEpoxyModel
 import com.tryden.nook.ui.epoxy.models.HeadingSectionTitleEpoxyModel
+import com.tryden.nook.ui.epoxy.models.SectionFooterRoundedEpoxyModel
+import com.tryden.nook.ui.epoxy.models.SectionHeaderTopRoundEpoxyModel
 import com.tryden.nook.ui.epoxy.models.priorities.SectionFolderItemEpoxyModel
 
 class HomeEpoxyController(
@@ -31,13 +29,16 @@ class HomeEpoxyController(
         HeadingPageTitleEpoxyModel("Folders").id("heading-folders").addTo(this)
 
         // Priorities Section
-        HeadingSectionTitleEpoxyModel("All Priorities").id("heading-priorities").addTo(this)
+        HeadingSectionTitleEpoxyModel("Priorities").id("heading-priorities").addTo(this)
+        SectionHeaderTopRoundEpoxyModel().id("section-priorities-header-top").addTo(this)
         SectionFolderItemEpoxyModel(
             0, // todo revisit
-            "Priorities",
+            "All Priorities",
             0,
             onFolderItemSelected
         ).id("folder-item-priorities").addTo(this)
+        SectionFooterRoundedEpoxyModel().id("section-priorities-footer-top").addTo(this)
+
     }
 
 
