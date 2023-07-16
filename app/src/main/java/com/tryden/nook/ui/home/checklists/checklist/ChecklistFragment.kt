0f7a6795082f1b07c656238cba99f5fb6b8ced36
Tyler Ryden <tryden11@gmail.com>
2023-07-16 15:30:38 -0400

    Need to come back to updating folder size when adding and deleting checklist item

--- app/src/main/java/com/tryden/nook/ui/home/checklists/checklist/ChecklistFragment.kt
+++ app/src/main/java/com/tryden/nook/ui/home/checklists/checklist/ChecklistFragment.kt
@@ -8,13 +8,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
+import com.airbnb.epoxy.EpoxyTouchHelper
 import com.tryden.nook.R
 import com.tryden.nook.database.entity.ChecklistItemEntity
 import com.tryden.nook.database.entity.FolderEntity
 import com.tryden.nook.databinding.FragmentChecklistBinding
+import com.tryden.nook.databinding.ModelChecklistItemEntityBinding
 import com.tryden.nook.ui.BaseFragment
 import com.tryden.nook.ui.BottomToolbarSetup
 import com.tryden.nook.ui.epoxy.models.BottomSheetViewType
+import com.tryden.nook.ui.epoxy.models.SectionFolderItemEpoxyModel
 
 class ChecklistFragment : BaseFragment() {
 
@@ -74,6 +77,41 @@ class ChecklistFragment : BaseFragment() {
             controller.itemEntityList = items as ArrayList<ChecklistItemEntity>
         }
 
+        swipeToDeleteSetup()
+
+    }
+
+    private fun swipeToDeleteSetup() {
+        EpoxyTouchHelper.initSwiping(binding.epoxyRecyclerView)
+            .right()
+            .withTarget(ChecklistItemEpoxyModel::class.java)
+            .andCallbacks(object : EpoxyTouchHelper.SwipeCallbacks<ChecklistItemEpoxyModel>() {
+
+                override fun onSwipeCompleted(
+                    model: ChecklistItemEpoxyModel?,
+                    itemView: View?,
+                    position: Int,
+                    direction: Int,
+                ) {
+                    val checklistItem = model?.itemEntity?: return
+
+                    sharedViewModel.deleteChecklistItem(checklistItem)
+
+                    // Decrease folder size by 1
+//                    val folderEntity = selectedFolderEntity!!.copy(
+//                        title = selectedFolderEntity!!.title,
+//                        type = selectedFolderEntity!!.type,
+//                        size = if (selectedFolderEntity!!.size > 0) {
+//                            selectedFolderEntity!!.size - 1
+//                        } else {
+//                            0
+//                        }
+//                    )
+//                    sharedViewModel.updateFolder(folderEntity)
+//                    Log.d(tag, "Folder ${selectedFolderEntity!!.title} size: ${selectedFolderEntity!!.size}")
+                }
+
+            })
     }
 
     override fun onDestroyView() {
